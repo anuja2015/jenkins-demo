65aaf023baacb00f15dff73ddd26bd4fce11c803
anu <anujamk2020@gmail.com>
2020-05-24 14:01:52 +0000

    fibo

---
+++ fibo.sh
@@ -0,0 +1,33 @@
+# Program for Fibonacci 
+# Series 
+
+# Static input fo N 
+
+#N=6 
+if [ $# -eq 1 ]
+then
+    Num=$1
+else
+    echo -n "Enter a Number :"
+    read Num
+fi
+
+# First Number of the 
+# Fibonacci Series 
+a=0 
+
+# Second Number of the 
+# Fibonacci Series 
+b=1 
+
+echo "The Fibonacci series is : "
+
+for (( i=0; i<Num; i++ )) 
+do
+	echo -n "$a "
+	fn=$((a + b)) 
+	a=$b 
+	b=$fn 
+done
+# End of for loop 
+
